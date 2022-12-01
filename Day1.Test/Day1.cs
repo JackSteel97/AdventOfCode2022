79dd3d31b8a33ebf1e26b285cb419201383b511c
Jack Steel <jacksteel@jacksteel.co.uk>
2022-12-01 15:57:53 +0000

    :sparkles: Day1

---
+++ Day1.Test/Day1.cs
@@ -0,0 +1,52 @@
+using FluentAssertions;
+
+namespace Day1.Test;
+
+public class Tests
+{
+    private string[] _input;
+    private string[] _exampleInput;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _input = File.ReadAllLines("input.txt");
+        _exampleInput = File.ReadAllLines("exampleInput.txt");
+    }
+
+    [Test]
+    public void Part1Example()
+    {
+        const int expectedAnswer = 24_000;
+        var actualResult = Day1.Part1(_exampleInput);
+
+        actualResult.Should().Be(expectedAnswer);
+    }
+    
+    [Test]
+    public void Part1Actual()
+    {
+        const int expectedAnswer = 66_306;
+        var actualResult = Day1.Part1(_input);
+
+        actualResult.Should().Be(expectedAnswer);
+    }
+    
+    [Test]
+    public void Part2Example()
+    {
+        const int expectedAnswer = 45_000;
+        var actualResult = Day1.Part2(_exampleInput);
+
+        actualResult.Should().Be(expectedAnswer);
+    }
+    
+    [Test]
+    public void Part2Actual()
+    {
+        const int expectedAnswer = 195_292;
+        var actualResult = Day1.Part2(_input);
+
+        actualResult.Should().Be(expectedAnswer);
+    }
+}
---
+++ Day1/Day1.cs
@@ -0,0 +1,60 @@
+﻿namespace Day1;
+
+public static class Day1
+{
+    public static int Part1(string[] input)
+    {
+        int currentMax = 0;
+        int elfRunningTotal = 0;
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                // Start a new Elf.
+                currentMax = Math.Max(currentMax, elfRunningTotal);
+                elfRunningTotal = 0;
+            }
+            else
+            {
+                elfRunningTotal += Convert.ToInt32(line);
+            }
+        }
+        currentMax = Math.Max(currentMax, elfRunningTotal);
+        return currentMax;
+    }
+    
+    public static int Part2(string[] input)
+    {
+        int elfRunningTotal = 0;
+        int[] top3Maxs = new[] { 0, 0, 0 };
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                // Start a new Elf.
+                TrackTopCarriers(top3Maxs, elfRunningTotal);
+                elfRunningTotal = 0;
+            }
+            else
+            {
+                elfRunningTotal += Convert.ToInt32(line);
+            }
+        }
+        TrackTopCarriers(top3Maxs, elfRunningTotal);
+        return top3Maxs.Sum();
+    }
+
+    private static void TrackTopCarriers(int[] top3Maxs, int elfRunningTotal)
+    {
+        
+        for (int i = 0; i < top3Maxs.Length; ++i)
+        {
+            if (elfRunningTotal > top3Maxs[i])
+            {
+                top3Maxs[i] = elfRunningTotal;
+                Array.Sort(top3Maxs);
+                break;
+            }
+        }
+    }
+}
